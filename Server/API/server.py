39d2ca3983488a2ee7ef3726e19aeea3e435c737
amin jamal <includeamin@MacBook-Pro.local>
2019-05-28 12:28:08 -0700

    add python file

--- Server/API/server.py
+++ Server/API/server.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from flask_cors import CORS
 from Bridges.RabbitMq import connection
 
@@ -7,7 +7,19 @@ CORS(app)
 
 @app.route('/task/assign')
 def  new_task():
-    pass
+    try:
+        pass
+    except Exception as ex:
+        return jsonify({"State":False,"Description":ex.args})
 @app.route('/task/checkout/<id>')
 def checkout(id):
-    pass
+    try:
+        pass
+    except Exception as ex:
+        return jsonify({"State":False,"Description":ex.args})
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port='3000',debug=True)
