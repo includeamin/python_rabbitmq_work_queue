36b5c16c9b18368dab294fccc563602aaed224f7
aminjamal <aminjamal10@gmail.com>
2019-05-28 18:05:52 +0430

    add docs

--- Server/API/server.py
+++ Server/API/server.py
@@ -1,6 +1,13 @@
 from flask import Flask,request
 from flask_cors import CORS
+from Bridges.RabbitMq import connection
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/task/assign')
+def  new_task():
+    pass
+@app.route('/task/checkout/<id>')
+def checkout(id):
+    pass
